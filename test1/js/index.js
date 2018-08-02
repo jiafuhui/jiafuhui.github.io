53dfbd37ed67dc0ca4eb45c5234353cb6416ed25
jfh <jiafuhui@dayima.com>
2018-08-02 12:13:41 +0800

    test

---
+++ test1/js/index.js
@@ -0,0 +1,21 @@
+$(function(){
+
+
+$.ajax({
+    url:"http://apps.bdimg.com/libs/jquery/2.1.4/jquery.min.js",
+    methods:"POST",
+    var html+="<div>"
+    success:function(e){
+        var schools=document.queryselector("school");
+        console.log(e);
+        for(var i=0;i<schools.length;i++){
+            html+='<p class="test">'++'</p>'
+            html+='<p class="test">'++'</p>'
+            html+='<p class="test">'++'</p>'
+            html+='<p class="test">'++'</p>'
+            html+='<p class="test">'++'</p>'      
+        }
+    }
+})
+
+})
